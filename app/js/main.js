38c6d1b2a7f730165d46d8385a6cb0c1e6f67928
DenysSemanchuk1 <semanchukdenys@gmail.com>
2021-12-20 14:15:48 +0200

    added main page

--- app/js/main.js
+++ app/js/main.js
@@ -1,31 +1,38 @@
-new Splide( '.splide', {
+new Splide(".splide", {
   classes: {
-		pagination: 'splide__pagination your-class-pagination',
-		page      : 'splide__pagination__page your-class-page',
-    
+    pagination: "splide__pagination your-class-pagination",
+    page: "splide__pagination__page your-class-page",
   },
   perPage: 3,
-  
-} ).mount();
-new Splide('.tv__slider', {
-  type : 'loop',
-  focus: 'center',
-  perPage: 3,
-  gap: '15rem',
 }).mount();
-$('.reviews__first-slider').slick({
+new Splide(".tv__slider", {
+  type: "loop",
+  focus: "center",
+  perPage: 3,
+  gap: "15rem",
+}).mount();
+$(".reviews__first-slider").slick({
   infinite: true,
   adaptiveHeight: true,
   variableWidth: true,
-  asNavFor: '.reviews__second-slider',
+  asNavFor: ".reviews__second-slider",
   arrows: false,
   focusOnSelect: true,
   centerMode: true,
   centerPadding: "50px",
+  slidesToShow: 3,
 });
-$('.reviews__second-slider').slick({
+$(".reviews__second-slider").slick({
   infinite: true,
-  asNavFor: '.reviews__first-slider',
-  centerMode: true,
+  slidesToShow: 1,
+  asNavFor: ".reviews__first-slider",
   focusOnSelect: true,
+  dots: true,
+  nextArrow:
+    '<button type="button" class="slick-next"><i><img src="images/next-icon.svg" alt=""></i></button>',
+  prevArrow:
+    '<button type="button" class="slick-prev"><i><img src="images/prev-icon.png" alt=""></i></button>',
+});
+$(".faq__item-title").on("click", function () {
+  $(this).toggleClass("active");
 });
